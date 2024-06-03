8b87363a809b1593d022350aea18f0bd04fb04aa
gozgir <gozgir@OFG>
2024-06-03 14:34:05 +0300

    ...

---
+++ CQRSProject/Controllers/CategoryMediatRController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CQRSProject.Controllers
+{
+    public class CategoryMediatRController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
